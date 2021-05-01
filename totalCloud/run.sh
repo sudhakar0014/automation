19c555bb4bf19c54c4d81be68edd82e3e4a96250
sudhakar0014 <37268539+sudhakar0014@users.noreply.github.com>
2021-05-01 05:37:22 +0530

    Update run.sh

--- totalCloud/run.sh
+++ totalCloud/run.sh
@@ -1,4 +1,3 @@
 ansible-playbook -i inventory.yml kube-install-software.yml 
 ansible-playbook -i inventory.yml kube-setup-cluster.yml 
-ansible-playbook -i inventory.yml kube-self-hosted-recovery.yml 
 ansible-playbook -i inventory.yml hello.yml 
